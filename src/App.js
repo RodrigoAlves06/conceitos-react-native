9580add8e52d2c2e15649c3901fb31fcaa787ca0
RodrigoAlves06 <rodrigohenriquealves1@gmail.com>
2020-06-23 20:32:25 -0300

    Commit-final, listando_api e executando a funçao curtir

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React ,  {useState , useEffect} from "react";
+
+import api from './services/api';
+
+//Listar os repositórios da sua API: Deve ser capaz de criar uma lista de todos os
+// repositórios que estão cadastrados na sua API com os campos title,
+// techs e número de curtidas seguindo o padrão ${repository.likes} curtidas,
+// apenas alterando o número para ser dinâmico.
+
+//Curtir um repositório listado da API: Deve ser capaz de curtir um item na sua API
+// através de um botão com o texto Curtir e deve atualizar o número de likes na listagem no mobile.
+
 
 import {
   SafeAreaView,
@@ -11,45 +22,94 @@ import {
 } from "react-native";
 
 export default function App() {
+
+  const [repositorios, setRepositorios] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response.data);
+      setRepositorios(response.data);
+    })
+  },[]);
+
+
+
+
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
+    const response = await api.post(`/repositories/${id}/like`);
+    
+    // const repositorio = response.data;
+    // setRepositorios([...repositorios , repositorio]);
+
+    // {repositorios.map ( repositorio => repositorio.likes)}
+
+    const {likes} = response.data;
+
+    setRepositorios([...repositorios.map((repositorio )=> {
+      if (repositorio.id === id ){
+        return {... repositorio, likes};
+      }
+      return repositorio;
+      
+    }),
+  ]);
+    
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        
+      <FlatList
+        data={repositorios}
+        keyExtractor={repositories => repositories.id}
+        renderItem={({item: repositories})=>(
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
+        <View style={styles.repositoryContainer}>
+          
+          
+            <Text style={styles.repository} 
+            >
+              {repositories.title}</Text>
+
+          
+          <View style={styles.techsContainer}>          
+            
+            {repositories.techs.map((tech)=>(
+              <Text style={styles.tech} key={tech}>
+                {tech}
+              </Text>
+            ))}
+            
+            {/* <Text style={styles.tech} >
+              {repositories.techs}
+            </Text> */}
           </View>
 
           <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
+            
+            <Text style={styles.likeText}
+            key={repositories.id}
+
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repositories.id}`}
             >
-              3 curtidas
+              {repositories.likes} curtidas
             </Text>
           </View>
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(1)}
+            onPress={() => handleLikeRepository(repositories.id)}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            testID={`like-button-${repositories.id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
-        </View>
+        </View>)}
+      />
       </SafeAreaView>
     </>
   );
